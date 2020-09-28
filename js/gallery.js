727674791d3aa17f37685b2c2758103b74970ad8
Roman Palna <roma.palna@gmail.com>
2020-09-28 16:47:35 +0300

    task finish

--- js/gallery.js
+++ js/gallery.js
@@ -2,8 +2,12 @@ import gallery from './gallery-items.js';
 
 // Додаємо галерею в HTML
 const galleryPic = document.querySelector('.js-gallery');
-const galleryMarkup = galleryCreate();
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxEl = document.querySelector('.lightbox__image');
+const closeModalBtn = document.querySelector('.lightbox__button');
+const overlayModal = document.querySelector('.lightbox__overlay');
 
+const galleryMarkup = galleryCreate();
 galleryPic.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function galleryCreate() {
@@ -26,4 +30,53 @@ function galleryCreate() {
     .join('');
 
   return markup;
+}
+
+//Делегування на галереї та отримання посилання на велике зображення
+galleryPic.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+
+//Відкриття модального вікна по кліку картинки галереї та підміна значень alt, src
+
+galleryPic.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightbox.classList.add('is-open');
+  lightboxEl.src = event.target.dataset.source;
+  lightboxEl.alt = event.target.alt;
+  document.addEventListener('keydown', onCloseModalByEsc);
+}
+
+closeModalBtn.addEventListener('click', onCloseModal);
+
+//Закриття модалки різними методами
+
+function onCloseModal() {
+  lightbox.classList.remove('is-open');
+  document.removeEventListener('keydown', onCloseModalByEsc);
+  lightboxEl.src = '';
+  closeModalByOverlay();
+}
+
+function onCloseModalByEsc(event) {
+  if (event.code === 'Escape') {
+    onCloseModal();
+  }
+}
+
+overlayModal.addEventListener('click', closeModalByOverlay);
+
+function closeModalByOverlay(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
 }
