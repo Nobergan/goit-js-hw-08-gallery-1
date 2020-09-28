29e87c89a63c700a47515d560cd4cb24525159dc
Roman Palna <roma.palna@gmail.com>
2020-09-28 14:18:45 +0300

    add gallery in HTML

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,29 @@
 import gallery from './gallery-items.js';
+
+// Додаємо галерею в HTML
+const galleryPic = document.querySelector('.js-gallery');
+const galleryMarkup = galleryCreate();
+
+galleryPic.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function galleryCreate() {
+  const markup = gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+
+  return markup;
+}
