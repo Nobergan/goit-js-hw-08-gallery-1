3d64de419d42a8eaa53f6156dfc65f418d5f0f74
Roman Palna <roma.palna@gmail.com>
2020-10-01 14:25:33 +0300

    upd

--- js/gallery.js
+++ js/gallery.js
@@ -6,6 +6,7 @@ const lightbox = document.querySelector('.js-lightbox');
 const lightboxEl = document.querySelector('.lightbox__image');
 const closeModalBtn = document.querySelector('.lightbox__button');
 const overlayModal = document.querySelector('.lightbox__overlay');
+const overlayContent = document.querySelector('.lightbox__content');
 
 const galleryMarkup = galleryCreate();
 galleryPic.insertAdjacentHTML('beforeend', galleryMarkup);
@@ -18,6 +19,7 @@ function galleryCreate() {
   <a
     class="gallery__link"
     href="${original}"
+    
   >
     <img
       class="gallery__image"
@@ -55,6 +57,8 @@ function onOpenModal(event) {
   lightbox.classList.add('is-open');
   lightboxEl.src = event.target.dataset.source;
   lightboxEl.alt = event.target.alt;
+  lightboxEl.dataset.index = event.target.dataset.index;
+
   document.addEventListener('keydown', onCloseModalByEsc);
 }
 
@@ -88,13 +92,40 @@ const leftBtn = document.querySelector('.move-left');
 const rightBtn = document.querySelector('.move-right');
 const imgGallery = document.querySelectorAll('.gallery__image');
 
-leftBtn.addEventListener('click', moveLeft);
+leftBtn.addEventListener('click', () => {
+  const currentOpenImageIndex = lightboxEl.dataset.index;
+  // Images indexes starts from 1, i.e. 1...9
+  // While indexes in array starts from 0, i.e 0...8
+  // So if we took image №2 in array it's under index №1
+  // So to get previou one we need to subtract 2 i.e. 2 - 2
+  const nextImage = gallery[currentOpenImageIndex - 2];
 
-function moveLeft() {
-  gallery.map((element, index) => {
-    index -= 1;
-    element === index;
+  console.log(currentOpenImageIndex);
 
-    lightboxEl.src = element.original;
-  });
-}
+  if (nextImage !== undefined) {
+    lightboxEl.src = nextImage.original;
+  }
+
+  // Check if it's first one
+  if (currentOpenImageIndex === 1) {
+    leftBtn.setAttribute('disabled');
+  } else {
+    leftBtn.removeAttribute('disabled');
+  }
+});
+
+rightBtn.addEventListener('click', () => {
+  const currentOpenImageIndex = lightboxEl.dataset.index;
+  const nextImage = gallery[currentOpenImageIndex];
+
+  if (nextImage !== undefined) {
+    lightboxEl.src = nextImage.original;
+  }
+
+  // Check if it's last one
+  if (currentOpenImageIndex === gallery.length) {
+    rightBtn.setAttribute('disabled');
+  } else {
+    rightBtn.removeAttribute('disabled');
+  }
+});
